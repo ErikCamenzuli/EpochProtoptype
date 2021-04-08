9a582d8e4b44f6aa71f9babe5dbedf2ab49c1c93
Erik <erik.camenzuli@hotmail.com>
2021-04-08 19:23:14 +1000

    Fixed player look

--- Epoch - Prototype/Assets/Scripts/Player/PlayerMove.cs
+++ Epoch - Prototype/Assets/Scripts/Player/PlayerMove.cs
@@ -7,38 +7,38 @@ public class PlayerMove : MonoBehaviour
 
     public CharacterController playerController;
     public float speed;
-    Vector3 velocity;
-    public float gravity;
-    public float velocityDrop;
+    Vector3 currentVelo;
+    public float gravity = -9.81f;
     public Transform groundCheck;
-    private float distanceToGround = 0.4f;
+    public float groundDist = 0.3f;
     public LayerMask groundMask;
     bool isGrounded;
-    public float jumpHeight;
+    public float jump;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, distanceToGround, groundMask);
-        if(isGrounded && velocity.y < 0)
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+        if(isGrounded && currentVelo.y < 0)
         {
-            velocity.y = velocityDrop;
+            currentVelo.y = -2f;
         }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = transform.right * x + transform.forward * z;
+  
         playerController.Move(moveDirection * speed * Time.deltaTime);
 
-        if(Input.GetButton("Jump") && isGrounded)
+        if(Input.GetButtonDown("Jump") && isGrounded )
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * velocityDrop * gravity);
+            currentVelo.y = Mathf.Sqrt(jump * -2f * gravity);
+            isGrounded = false;
         }
 
-        velocity.y -= gravity * Time.deltaTime;
-        playerController.Move(velocity * Time.deltaTime);
-
-
+        currentVelo.y += gravity * Time.deltaTime;
+        playerController.Move(currentVelo * Time.deltaTime);
     }
 }
